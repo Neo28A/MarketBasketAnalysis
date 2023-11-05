2448cdd368eee41c4a67af6d279082c61afe5dd4
Chetan M  Kittali <102372671+Neo28A@users.noreply.github.com>
2023-11-05 14:09:25 +0530

    Update marketbasketanalysisusingaprior.py

--- MarketBasketAnalysis/marketbasketanalysisusingaprior.py
+++ MarketBasketAnalysis/marketbasketanalysisusingaprior.py
@@ -11,7 +11,7 @@ Original file is located at
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
+
 
 # @title Uploading Dataset
 from google.colab import files
